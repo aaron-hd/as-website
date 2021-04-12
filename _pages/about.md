9a759d90f8256112ace1baf0286f2a3d3c76b182
aaron-hd <held.aaron@googlemail.com>
2021-04-13 00:06:44 +0100

    missing files from last commit

--- _pages/about.md
+++ _pages/about.md
@@ -1,6 +1,7 @@
 ---
 permalink: /about/
-title: "10 years of asymptotic safety seminars"
+title: "10 years of Asymptotic Safety Seminars"
+classes: wide
 ---
 
 This is where we write a little bit about what has happened and what we intend with the series ...
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,25 @@
 var store = [{
-        "title": "Sample Speaker: sample title",
-        "excerpt":"Abstract: This is a sample abstract. It will go on for a couple of lines … This is a sample abstract. It will go on for a couple of lines … This is a sample abstract. It will go on for a couple of lines … This is a sample...","categories": [],
+        "title": "Alicia Castro: FRG Approach to the Continuum Limit of Matrix Models of Quantum Gravity with Preferred Foliation",
+        "excerpt":"In this talk I will present the analysis of a dually-weighted Matrix Model which implements a preferred foliation in its dual-graph. By using the FRGE, we study the existence of fixed points and its universality class (CDT or Euclidean Matrix Models). We find first evidence that the FGRE is a...","categories": [],
         "tags": [],
-        "url": "/2010/01/07/post-standard.html",
+        "url": "/2020/03/30/Alicia-Castro.html",
         "teaser": null
       },{
-        "title": "Sample Speaker: sample title",
-        "excerpt":"Abstract: This is a sample abstract. It will go on for a couple of lines … This is a sample abstract. It will go on for a couple of lines … This is a sample abstract. It will go on for a couple of lines … This is a sample...","categories": [],
+        "title": "Seth Kurankyi Asante: Effective spin foam models for four dimensional quantum gravity",
+        "excerpt":"A number of approaches to four-dimensional quantum gravity, such as loop quantum gravity and holography, situate areas as their fundamental variables. These area variables are however, not free but satisfy certain constraints which allow for a consistent gluing. I will introduce a family of ‘effective’ spin foam models that incorporate...","categories": [],
         "tags": [],
-        "url": "/2021/03/30/post-sample-speaker.html",
+        "url": "/2021/02/22/Seth-Asante.html",
+        "teaser": null
+      },{
+        "title": "Raúl Carballo Rubio: Mass inflation and the semiclassical limit of quantum black holes",
+        "excerpt":"Geometries describing static non-singular black holes have been obtained in suitable limits of various quantum gravity frameworks, including asymptotic safety. After a brief review of the underlying geometric reasons behind this universality, and a discussion of the similarities between these geometries and classical charged/rotating black holes, the talk will focus...","categories": [],
+        "tags": [],
+        "url": "/2021/03/29/Raul-Carballo-Rubio.html",
+        "teaser": null
+      },{
+        "title": "Judah Unmuth-Yockey: Newtonian binding from lattice quantum",
+        "excerpt":"We consider a free scalar field on Euclidean dynamical triangulations. By studying the 1-particle and 2-particle correlation functions, we extract the binding energy between two scalar particles. We extrapolate our data for the binding energy to the continuum, infinite volume limit, and find a result consistent with the expected power-law...","categories": [],
+        "tags": [],
+        "url": "/2021/04/12/post-Judah-Unmuth-Yockey.html",
         "teaser": null
       }]
